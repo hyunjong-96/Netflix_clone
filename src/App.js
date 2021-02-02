e10ad233c81bd2469eb3648b8b821f33388a2c0e
hyunjong_96 <ahajongs@gmail.com>
2021-02-03 04:46:35 +0900

    #2 MainBoard_Header 배경화면 그라데이션작업중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {createGlobalStyle} from 'styled-components'
 import MenuNavigationContainer from './containers/MenuNavigationContainer'
-
+import MainBoard_HeaderContainer from './containers/MainBoard_HeaderContainer'
 const GlobalStyle = createGlobalStyle`
   body{
     background:#141414;
@@ -13,6 +13,7 @@ function App() {
     <>
     <GlobalStyle/>
     <MenuNavigationContainer/>
+    <MainBoard_HeaderContainer/>
     <div style={{overflowY:"scroll"}}></div>
     <div style={{color:'white'}}>hi</div>
     <div style={{color:'white'}}>hi</div>
--- src/components/MainBoard_Header.js
+++ src/components/MainBoard_Header.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import eventPoster from '../static/images/logo.png'
+
+const MainBoard_Img=styled.div`
+    position:absolute;
+  width: 100%;
+  height: 950px;
+  background: linear-gradient(
+      to bottom,
+      rgba(20, 20, 20, 0.3) 10%,
+      rgba(20, 20, 20, 1) 90%,
+      rgba(20, 20, 20, 1)
+    ),
+    url(${eventPoster});
+  //background-size: cover;
+`
 
 function MainBoard_Header(){
-
+    return(
+        <MainBoard_Img>
+            {/* <img className='mainBoard_poster' src={eventPoster}/> */}
+        </MainBoard_Img>
+    )
 }
 
 export default MainBoard_Header
